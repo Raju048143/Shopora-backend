512f85463ab6e5881d8cd07ace20ed83e3c79c5d
RAJU YADAV <rajuyadav91391@gmail.com>
2025-08-21 22:48:58 +0530

    Fixed import isssue

--- Routes/AuthRouter.js
+++ Routes/AuthRouter.js
@@ -1,7 +1,7 @@
 import express from "express";
 import {signupValidation, loginValidation}  from "../Middlewares/AuthValidation.js";
 import signup  from "../Controllers/AuthController.js";
-import signin from "../Controllers/signinController.js";
+import signin from "../Controllers/SigninController.js";
 const router = express.Router();
 
 router.post("/login", loginValidation, signin);
