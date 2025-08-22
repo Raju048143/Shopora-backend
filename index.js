c623cf17625ceaddd44dcd7c4e0c8e246401862c
RAJU YADAV <rajuyadav91391@gmail.com>
2025-08-22 10:59:37 +0530

    udpate frontend url

--- index.js
+++ index.js
@@ -8,7 +8,10 @@ const PORT = process.env.PORT || 3000;
 
 // Middleware
 app.use(bodyParser.json());
-app.use(cors());
+app.use(cors({
+  origin: process.env.FRONTNED_URL,
+  credentials: true
+}));
 
 // Routes
 app.use("/auth", AuthRouter);
