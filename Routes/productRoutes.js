f7dc6035dce50e9673141ea1962c02fe02cc888f
RAJU YADAV <rajuyadav91391@gmail.com>
2025-08-27 07:12:06 +0530

    import

--- Routes/productRoutes.js
+++ Routes/productRoutes.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { createProduct , getProducts} from "../controllers/productController.js";
+import { createProduct , getProducts} from "../Controllers/productController.js";
 
 const router = express.Router();
 
